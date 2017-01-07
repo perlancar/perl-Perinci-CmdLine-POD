1e0b568db349b0a6ad0ab4dd1f81fd64e1faa7d3
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2017-01-07 17:51:47 +0700

    WIP

--- lib/Perinci/CmdLine/POD.pm
+++ lib/Perinci/CmdLine/POD.pm
@@ -7,8 +7,99 @@ use 5.010001;
 use strict;
 use warnings;
 
+our %SPEC;
+
+$SPEC{gen_pod_for_pericmd_script} = {
+    v => 1.1,
+    summary => 'Generate POD for Perinci::CmdLine-based CLI script',
+    description => <<'_',
+
+This utility can accept either a path to a <pm:Perinci::CmdLine>-based CLI
+script, upon which the arguments to Perinci::CmdLine constructor will be
+extracted using a script dumper (<pm:Perinci::CmdLine::Dump>), or a set of
+arguments to specify Perinci::CmdLine constructor arguments directly (e.g.
+`url`, `summary`, `subcommands`, etc).
+
+_
+    args => {
+        script => {
+            summary => 'Path to script',
+            schema => 'filename*',
+            tags => ['category:input'],
+        },
+
+        url => {
+            schema => 'str*',
+            tags => ['category:input'],
+        },
+        program_name => {
+            schema => 'str*',
+            tags => ['category:input'],
+        },
+        summary => {
+            schema => 'str*',
+            tags => ['category:input'],
+        },
+        subcommands => {
+            schema => ['hash*', of=>'hash*'],
+            tags => ['category:input'],
+        },
+        default_subcommand => {
+            schema => 'str*',
+            tags => ['category:input'],
+        },
+        per_arg_json => {
+            schema => ['bool*', is=>1],
+            default => 1,
+            tags => ['category:input'],
+        },
+        per_arg_yaml => {
+            schema => ['bool*', is=>1],
+            default => 1,
+            tags => ['category:input'],
+        },
+        read_env => {
+            schema => ['bool*', is=>1],
+            default => 1,
+            tags => ['category:input'],
+        },
+        env_name => {
+            schema => 'str*',
+            tags => ['category:input'],
+        },
+        read_config => {
+            schema => ['bool*', is=>1],
+            default => 1,
+            tags => ['category:input'],
+        },
+        config_filename => {
+            schema => ['any*', of=>[
+                'str*',
+                ['array*', of=>'str*'],
+                ['array*', of=>'hash*'],
+            ]],
+            tags => ['category:input'],
+        },
+        config_dirs => {
+            schema => ['array*', of=>'dirname*'],
+            tags => ['category:input'],
+        },
+    },
+    args_rels => {
+        req_one => [qw/script url/],
+    },
+};
+sub gen_pod_for_pericmd_script {
+    my %args = @_;
+
+    if (defined $args{script}) {
+        require Perinci::CmdLine::Dump;
+        my $res = Perinci::CmdLine::Dump::dump_
+    }
+}
+
 1;
-# ABSTRACT: Generate POD for Perinci::CmdLine-based CLI script
+# ABSTRACT:
 
 =head1 SYNOPSIS
 
