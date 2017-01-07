dce036ae0469e69f05163e33dee19888be155484
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2017-01-07 07:16:32 +0700

    Initial commit

---
+++ lib/Perinci/CmdLine/POD.pm
@@ -0,0 +1,23 @@
+package Perinci::CmdLine::POD;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: Generate POD for Perinci::CmdLine-based CLI script
+
+=head1 SYNOPSIS
+
+Please see the included CLI script L<gen-pod-for-pericmd-script>.
+
+
+=for Pod::Coverage ^(new)$
+
+
+=head1 SEE ALSO
+
+L<Perinci::CmdLine>
